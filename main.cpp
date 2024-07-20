9acd9d4fa45651b801f2cb0bbc5e488f0d23897e
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-20 17:14:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 int main() {
+    // Original test cases
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
