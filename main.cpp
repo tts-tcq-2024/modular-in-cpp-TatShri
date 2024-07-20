278e836fc3dcebb5d4f52bc50480f28bb6edd7bc
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-20 16:56:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,10 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
+    //If reference manual needs to be printed
+    /*
+    std::string manual = TelCoColorCoder::GetReferenceManual();
+    std::cout << manual;
+    */
     return 0;
 }
