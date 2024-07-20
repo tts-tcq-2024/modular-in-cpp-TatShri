30f481d251e3dc13a0a013eafc845753dc75932c
Shriya Tatachar <62716775+TatShri@users.noreply.github.com>
2024-07-20 16:52:24 +0530

    Update referencemanual.cpp

--- referencemanual.cpp
+++ referencemanual.cpp
@@ -1,1 +1,16 @@
-
+#include "colorcoder.h"
+#include <sstream>
+//Reference manual for wiring personnel
+namespace TelCoColorCoder {
+    std::string GetReferenceManual() {
+        std::ostringstream manual;
+        for (int majorIndex = 0; majorIndex < numberOfMajorColors; ++majorIndex) {
+            for (int minorIndex = 0; minorIndex < numberOfMinorColors; ++minorIndex) {
+                ColorPair colorPair = ColorPair((MajorColor)majorIndex, (MinorColor)minorIndex);
+                int pairNumber = GetPairNumberFromColor((MajorColor)majorIndex, (MinorColor)minorIndex);
+                manual << pairNumber << ": " << colorPair.ToString() << "\n";
+            }
+        }
+        return manual.str();
+    }
+}
